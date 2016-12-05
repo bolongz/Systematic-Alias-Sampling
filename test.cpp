9e1d04f97b9cacf95472e62389556a9e4e582d9d
YOUR NAME <blzhang.m@gmail.com>
2016-12-04 22:54:43 -0500

    add test

--- test.cpp
+++ test.cpp
@@ -4,6 +4,8 @@
 #include <iomanip>
 #include <random>
 #include <cmath>
+#include <sys/time.h>
+#include <ctime>
 
 const double PI = 3.14159265359;
 
@@ -12,6 +14,16 @@ double _normal_distribution(double mean, double dev, double x){
     
     return 1.0/(sqrt(2 * PI) * dev) * exp((-1.0/2.0) * pow((x - mean) / dev, 2));
 }
+    
+void __normal_distribution(int samplecount){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::normal_distribution<double> _normal_distribution(0.0, 1.0);
+    for(int i = 0; i <samplecount; i++) {
+        _normal_distribution(gen);
+    }
+}
+    
 
 int main(int argc, char *argv[]){
 
@@ -32,7 +44,7 @@ int main(int argc, char *argv[]){
     //std::random_device rd;
     //std::mt19937 gen(rd());
     //std::normal_distribution<double> _normal_distribution(0.0, 1.0);
-    
+    struct timeval tvs, tvm; 
     std::vector<double> pmf;
     std::vector<double> values;
     double h = (8.0) / bincount; //[take value in [-4, 4]
@@ -41,22 +53,68 @@ int main(int argc, char *argv[]){
         values.push_back(t);
         pmf.push_back(_normal_distribution(0.0, 1.0, t));
     }
-
     SystematicAliasSampling _sample(pmf, values);
     
-    _sample.aliastable(); //generate table
-
+    //for(int i = 0; i < bincount +1 ; i++){
+    
+    //    std::cout << _sample.values[i] << std::endl;
+    
+    //}
     
     std::vector<double> samples(samplecount);
-    _sample.systematicaliassampling(samplecount, samples);
-    //std::vector<double> samples2(samplecount);
-   
-    //_sample.goldratioaliassampling(samplecount, samples2);
     
-    for(int i = 0; i < samplecount ; i++){
-        std::cout << samples[i]<< std::endl;
-    }
+    _sample.aliastable(); //generate table
+    gettimeofday(&tvs, NULL);
+    _sample.systematicaliassampling(samplecount, samples);
+    gettimeofday(&tvm, NULL);
+    
+    double span1 = tvm.tv_sec-tvs.tv_sec + (tvm.tv_usec-tvs.tv_usec)/1000000.0;
+    std::cout << "SAS cost: " << span1 <<std::endl;
+    
+    gettimeofday(&tvs, NULL);
+    __normal_distribution(samplecount);
+    gettimeofday(&tvm, NULL);
+    double span2 = tvm.tv_sec-tvs.tv_sec + (tvm.tv_usec-tvs.tv_usec)/1000000.0;
+    std::cout << "normal_distribution cost: " << span2 <<std::endl;
 
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::discrete_distribution<> _discrete_distribution(pmf.begin(), pmf.end());
+    gettimeofday(&tvs, NULL);
+   
+    std::vector<double> __p = _discrete_distribution.probabilities();
+
+    /*for(int i = 0 ; i <bincount + 1; i++){
+    
+        std::cout << pmf[i] << " "<< _sample.pmf[i] << " "<< __p[i] << std::endl; 
+    }
+    */
+    for(int i = 0 ; i < samplecount; i++){
+        _discrete_distribution(gen);
+        
+    }
+    gettimeofday(&tvm, NULL);
+    double span3 = tvm.tv_sec-tvs.tv_sec + (tvm.tv_usec-tvs.tv_usec)/1000000.0;
+    std::cout << "discrete_distribution cost: " << span3 <<std::endl;
+    //std::vector<double> samples2(samplecount);
+    //_sample.goldratioaliassampling(samplecount, samples2);
+    /*
+    std::vector<double> edf(bincount + 1);
+    std::vector<double> cdf(bincount + 1);
+
+    _sample.empirical_distribution(samples, edf);
+    _sample.cummulative_distribution(cdf);
+    std::sort(samples2.begin(), samples2.end());
+    */
+    for(int i = 0; i < samplecount ; i++){
+      //  std::cout << samples[i]<< std::endl;
+    }
+    // for(int i = 0; i < bincount + 1 ;i++){
+    //    std::cout << cdf[i] << std::endl;
+    //}
+    //
+    //double cvm = _sample.cramer_von_mises(cdf, edf);
+    //std::cout << cvm <<std::endl;
     return 0; 
 
 }
